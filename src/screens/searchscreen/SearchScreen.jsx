a7309cebc5d6c68bfef9f87a8e128adbe476de09
Usman Sagheer <apnausmansagheer@gmail.com>
2024-12-19 12:50:35 +0500

    feat:setup svg and set in bottom tab

--- src/screens/searchscreen/SearchScreen.jsx
+++ src/screens/searchscreen/SearchScreen.jsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { DashboardIcon } from '../../assets/svg/index';
 
 const SearchScreen = () => {
   return (
     <View>
       <Text>SearchScreen</Text>
+      <DashboardIcon width={24} height={24} fill="black" />
     </View>
-  )
-}
+  );
+};
 
+// eslint-disable-next-line semi
 export default SearchScreen
